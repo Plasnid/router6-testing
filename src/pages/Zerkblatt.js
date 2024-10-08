ebc97bfca5c78ad9dfae76f2832ffb0936108fe2
Plasnid <stuart@sackler.ca>
2024-10-08 11:29:38 -0400

    updated example to show css and image imports

--- src/pages/Zerkblatt.js
+++ src/pages/Zerkblatt.js
@@ -1,7 +1,10 @@
+import Alien from './tree_alien.webp';
+import './zerkblatt.css';
 const Zerkblatt = () => {
     return (
         <>
             <p>Zerkblatt is a wonderfully tropical place with friendly aliens.</p>
+            <img src={Alien} className="treeAlien"></img>
         </>
     );
 }
